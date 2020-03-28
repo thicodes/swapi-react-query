0c8fc013ea83b920753caa9d4bb6c4664f2f322e
thiagoleitedev <thiagoleitebr@gmail.com>
2020-03-27 22:47:51 -0300

    Add body for global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import Characters from './components/Characters';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background: #4a5b9a;
+    font-family: Arial;
+  }
+`;
 function App() {
-  return <div className="App"></div>;
+      <GlobalStyle />
 }
 
 export default App;
